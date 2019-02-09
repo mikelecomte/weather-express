f33305c8247806220c689b4af69e579eb10ff9b4
Mike Lecomte <m.lecomte@gmail.com>
2019-02-09 15:44:12 -0500

    progress!

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 const express = require("express");
 const app = express();
 const request = require("request");
-const owmKey = '7e48a63137c1d244bb76c280ad6f9f1d';
+const owmKey = "7e48a63137c1d244bb76c280ad6f9f1d";
 
-app.use(express.json());
+app.use(function(req, res, next) {
+  console.log("Time:", Date.now());
+  console.log("Req URL:", req.url);
+  next();
+});
 
 const port = process.env.port || 3000;
 
-app.get('/api/weather', (req, res) => {
-    request.get(`http://api.openweathermap.org/data/2.5/weather?q=Guelph&APPID=${owmKey}`, (error, response, body) => {
+app.get("/api/weather/current/:location", (req, res) => {
+  request.get(
+    `http://api.openweathermap.org/data/2.5/weather?q=${req.params.location}&units=metric&APPID=${owmKey}`,
+    (error, response, body) => {
+      if (response.statusCode === 404) {
+        return res.status(404).send("City not found");
+      }
 
-    if(error) {
-        return res.send(error);
+      res.status(200).json(body);
+      console.log("Current weather sent for", req.params.location);
     }
+  );
+});
 
-    res.send(JSON.parse(body));
-  });
-  
+app.get("/*", (req, res) => {
+  res.status(400).send("Invalid request");
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
