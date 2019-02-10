c802203b6af01c81b4434e8634a22282a1e229ce
Mike Lecomte <m.lecomte@gmail.com>
2019-02-09 20:28:50 -0500

    prettier changes

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const request = require('request');
-const HttpStatus = require('http-status-codes');
+const request = require("request");
+const HttpStatus = require("http-status-codes");
 
-const owmKey = '7e48a63137c1d244bb76c280ad6f9f1d';
+const owmKey = "7e48a63137c1d244bb76c280ad6f9f1d";
 const port = process.env.port || 3000;
 
 app.use(function(req, res, next) {
-  console.log('Time:', Date.now());
-  console.log('Req URL:', req.url);
+  console.log("Req URL:", req.url);
   next();
 });
 
-app.get('/api/weather/current/:location', (req, res) => {
+app.get("/api/weather/current/:location", (req, res) => {
   request.get(
-    `http://api.openweathermap.org/data/2.5/weather?q=${req.params.location}&units=metric&APPID=${owmKey}`,
+    `http://api.openweathermap.org/data/2.5/weather?q=${
+      req.params.location
+    }&units=metric&APPID=${owmKey}`,
     (error, response, body) => {
-      console.log('Res status code:', response.statusCode);
+      console.log("Res code:", response.statusCode);
 
       if (response.statusCode === HttpStatus.NOT_FOUND) {
-        return res.status(HttpStatus.NOT_FOUND).send(`City not found: ${req.params.location}`);
+        return res
+          .status(HttpStatus.NOT_FOUND)
+          .send(`City not found: ${req.params.location}`);
       }
 
       if (response.statusCode !== HttpStatus.OK) {
-        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send('Something went wrong!');
+        return res
+          .status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .send("Something went wrong!");
       }
 
       res.status(HttpStatus.OK).json(body);
@@ -31,8 +36,8 @@ app.get('/api/weather/current/:location', (req, res) => {
   );
 });
 
-app.get('/*', (req, res) => {
-  res.status(400).send('Invalid request');
+app.get("/*", (req, res) => {
+  res.status(HttpStatus.BAD_REQUEST).send("Invalid request");
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
