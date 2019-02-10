64dd11bdb45469788d90cf176bc6a451d8ddf40a
Mike Lecomte <m.lecomte@gmail.com>
2019-02-09 20:00:18 -0500

    added http status code package, cleaned up formatting

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const request = require("request");
-const owmKey = "7e48a63137c1d244bb76c280ad6f9f1d";
+const request = require('request');
+const HttpStatus = require('http-status-codes');
+
+const owmKey = '7e48a63137c1d244bb76c280ad6f9f1d';
+const port = process.env.port || 3000;
 
 app.use(function(req, res, next) {
-  console.log("Time:", Date.now());
-  console.log("Req URL:", req.url);
+  console.log('Time:', Date.now());
+  console.log('Req URL:', req.url);
   next();
 });
 
-const port = process.env.port || 3000;
-
-app.get("/api/weather/current/:location", (req, res) => {
+app.get('/api/weather/current/:location', (req, res) => {
   request.get(
     `http://api.openweathermap.org/data/2.5/weather?q=${req.params.location}&units=metric&APPID=${owmKey}`,
     (error, response, body) => {
-      if (response.statusCode === 404) {
-        return res.status(404).send("City not found");
+      console.log('Res status code:', response.statusCode);
+
+      if (response.statusCode === HttpStatus.NOT_FOUND) {
+        return res.status(HttpStatus.NOT_FOUND).send(`City not found: ${req.params.location}`);
       }
 
-      res.status(200).json(body);
-      console.log("Current weather sent for", req.params.location);
+      if (response.statusCode !== HttpStatus.OK) {
+        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send('Something went wrong!');
+      }
+
+      res.status(HttpStatus.OK).json(body);
     }
   );
 });
 
-app.get("/*", (req, res) => {
-  res.status(400).send("Invalid request");
+app.get('/*', (req, res) => {
+  res.status(400).send('Invalid request');
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
