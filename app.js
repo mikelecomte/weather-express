7fc9de65838a1ae14dbfc724b2c75633a3fd4a41
Mike Lecomte <m.lecomte@gmail.com>
2019-02-11 20:12:04 -0500

    add forecast, create custom JSON object

--- app.js
+++ app.js
@@ -32,7 +32,59 @@ app.get("/api/weather/current/:location", (req, res) => {
           .send("Something went wrong!");
       }
 
-      res.status(HttpStatus.OK).json(body);
+      const owmData = JSON.parse(body);
+
+      const output = {
+        city: owmData.name,
+        weather: owmData.weather[0].main,
+        weatherDesc: owmData.weather[0].description,
+        currentTemp: owmData.main.temp,
+        humidity: owmData.main.humidity,
+        pressure: owmData.main.pressure,
+        windSpeed: owmData.wind.speed
+      };
+
+      res.json(output);
+    }
+  );
+});
+
+app.get("/api/weather/forecast/:location", (req, res) => {
+  request.get(
+    `http://api.openweathermap.org/data/2.5/forecast?q=${
+      req.params.location
+    }&units=metric&APPID=${owmKey}`,
+    (error, response, body) => {
+      console.log("Res code:", response.statusCode);
+
+      if (response.statusCode === HttpStatus.NOT_FOUND) {
+        return res
+          .status(HttpStatus.NOT_FOUND)
+          .send(`City not found: ${req.params.location}`);
+      }
+
+      if (response.statusCode !== HttpStatus.OK) {
+        return res
+          .status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .send("Something went wrong!");
+      }
+
+      const owmData = JSON.parse(body);
+      let output = [];
+
+      for (day of owmData.list) {
+        output.push({
+          day: new Date(day.dt * 1000),
+          weather: day.weather[0].main,
+          weatherDesc: day.weather[0].description,
+          temp: day.main.temp,
+          humidity: day.main.humidity,
+          pressure: day.main.pressure,
+          windSpeed: day.wind.speed
+        });
+      }
+
+      res.json(output);
     }
   );
 });
