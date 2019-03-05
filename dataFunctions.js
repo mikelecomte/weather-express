5282299825d292c9cb9e997a3b916dccdab2eebd
Mike Lecomte <m.lecomte@gmail.com>
2019-03-04 22:54:18 -0500

    add rain, snow, icon

--- dataFunctions.js
+++ dataFunctions.js
@@ -4,10 +4,13 @@ module.exports = {
       day: owmData.dt,
       weather: owmData.weather[0].main,
       weatherDesc: owmData.weather[0].description,
+      icon: owmData.weather[0].icon,
       temp: owmData.main.temp,
       humidity: owmData.main.humidity,
       pressure: owmData.main.pressure,
-      windSpeed: owmData.wind.speed
+      windSpeed: owmData.wind.speed,
+      rain: owmData.rain && owmData.rain["3h"] ? owmData.rain["3h"] : null,
+      snow: owmData.snow && owmData.snow["3h"] ? owmData.snow["3h"] : null
     };
   }
 };
